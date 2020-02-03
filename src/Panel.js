7be524b5acdad862eeb08f3d6d4e7280e674b2a3
Levron Jérémy <jeremylevron@19h47.fr>
2020-02-03 16:34:29 +0100

    :recycle: Small refacto

--- src/Panel.js
+++ src/Panel.js
@@ -1,22 +1,4 @@
-
-/**
- * Inactive element
- *
- * @param	obj		element		DOM element
- * @access	static
- * @return	void
- */
-export const setInactive = element => element.classList.remove('is-active');
-
-
-/**
- * Active element
- *
- * @param	obj		element		DOM element
- * @access	static
- * @return 	void
- */
-export const setActive = element => element.classList.add('is-active');
+import { setInactive, setActive } from '@/utils';
 
 
 const EXPANDED = 'aria-expanded';
