82f60c90f3ff3e42671d104db0d25ce77558826a
19h47 <jeremylevron@19h47.fr>
2020-07-20 07:41:33 +0200

    :arrow_up: Up

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,32 +4,28 @@ module.exports = {
 		node: true,
 		browser: true,
 	},
-	extends: [
-		'standard',
-		'airbnb-base',
-		'prettier'
-	],
+	extends: ['standard', 'airbnb-base', 'prettier'],
 	rules: {
 		'arrow-parens': ['error', 'as-needed'],
 		'no-console': 'off',
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': 'production' === process.env.NODE_ENV ? 'error' : 'off',
 		'no-tabs': 0,
-		indent: ['error', 'tab', { SwitchCase: 1, ignoredNodes: ["TemplateLiteral"] }],
-		"template-curly-spacing": ["off"],
+		indent: ['error', 'tab', { SwitchCase: 1, ignoredNodes: ['TemplateLiteral'] }],
+		'template-curly-spacing': ['off'],
 		'no-param-reassign': ['error', { props: false }],
 		yoda: [2, 'always'],
-		"import/no-named-as-default": 0
+		'import/no-named-as-default': 0,
 	},
 	parser: 'babel-eslint',
 	parserOptions: {
 		sourceType: 'module',
-		allowImportExportEverywhere: true
+		allowImportExportEverywhere: true,
 	},
 	settings: {
 		'import/resolver': {
-			'webpack': {
-				'config': 'config/webpack.common.js'
+			webpack: {
+				config: 'config/webpack.common.js',
 			},
-		}
+		},
 	},
 };
